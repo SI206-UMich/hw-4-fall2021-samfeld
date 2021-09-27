b415f9c02df24dc6d40a7816474d14b0873913c4
samfeld <90571485+samfeld@users.noreply.github.com>
2021-09-27 19:33:36 -0400

    Fixed errors
    Fixed 2 errors in test cases (submit order method and  make payment fixed)

--- hw4.py
+++ hw4.py
@@ -32,6 +32,7 @@ class Customer:
         self.cashier=cashier
         self.stall=stall
         self.amount=amount
+        self.wallet-=amount
         self.cashier.receive_payment(self.stall, self.amount)
 
     # The __str__ method prints the customer's information.    
@@ -70,7 +71,6 @@ class Cashier:
     
     # string function.
     def __str__(self):
-
         return "Hello, this is the " + self.name + " cashier. We take preloaded market payment cards only. We have " + str(sum([len(category) for category in self.directory.values()])) + " vendors in the farmers' market."
 
 ## Complete the Stall class here following the instructions in HW_4_instructions_rubric
@@ -87,27 +87,28 @@ class Stall:
     
     def has_item(self, name, quantity):
         self.name=name
-        self.quantity=quantity
+        #self.quantity=quantity
         if self.name in self.invenotry:
-            if self.invenotry[self.name]>=self.quantity:
+            if self.invenotry[self.name]>=quantity:
                 return True
+            else:
+                return False
         else:
-            return False
-    
+            return None
     def stock_up(self, name, quantity):
         self.name=name
-        self.quantity=quantity
+        #self.quantity=quantity
         if self.name in self.invenotry:
-            self.invenotry[self.name]+=self.quantity
+            self.invenotry[self.name]+=quantity
         else:
-            self.invenotry[self.name]=self.quantity
+            self.invenotry[self.name]=quantity
     
     def compute_cost(self, quantity):
         self.quantity=quantity
         return self.quantity*self.cost
 
     def __str__(self):
-        return "Hello, we are " +self.name+". This is the current menu "+self.inventory.keys()+". We charge $" + self.cost+" per item. We have $" + self.earnings+ " in total."
+        return "Hello, we are " +self.name+". This is the current menu "+self.invenotry.keys()+". We charge $" + self.cost+" per item. We have $" + self.earnings+ " in total."
 
 class TestAllMethods(unittest.TestCase):
     
@@ -191,7 +192,7 @@ class TestAllMethods(unittest.TestCase):
         # Test to see if has_item returns True when a stall has enough items left
         # Please follow the instructions below to create three different kinds of test cases 
         # Test case 1: the stall does not have this food item: 
-        self.assertEqual(self.s1.has_item("Pizza", 20), False)
+        self.assertEqual(self.s1.has_item("Pizza", 20), None)
         # Test case 2: the stall does not have enough food item: 
         self.assertEqual(self.s1.has_item("Burger", 80), False)
         # Test case 3: the stall has the food item of the certain quantity: 
@@ -200,15 +201,17 @@ class TestAllMethods(unittest.TestCase):
 	# Test validate order
     def test_validate_order(self):
 		# case 1: test if a customer doesn't have enough money in their wallet to order
-        self.assertEqual(self.f1.validate_order(self.c1, self.s1, "Burger", 12), "Don't have enough money for that :( Please reload more money!")
+        self.assertEqual(self.f1.validate_order(self.c2, self.s2, "Taco", 12), "Don't have enough money for that :( Please reload more money!")
 		# case 2: test if the stall doesn't have enough food left in stock
         self.assertEqual(self.f1.validate_order(self.c1, self.s1, "Burger", 41), "Our stall has run out of " + self.item_name + " :( Please try a different stall!")
 		# case 3: check if the cashier can order item from that stall
-        self.assertEqual(self.f1.validate_order(self.c2, self.s2, "Burger", 41), "Sorry, we don't have that vendor stall. Please try a different one.")
+        self.assertEqual(self.f1.validate_order(self.c2, self.s2, "Burger", 5), "Sorry, we don't have that vendor stall. Please try a different one.")
 
     # Test if a customer can add money to their wallet
     def test_reload_money(self):
-        self.assertEqual(self.f2.reload_money(10), 160)
+        original_amount=self.f2.wallet
+        self.f2.reload_money(10)
+        self.assertEqual(self.f2.wallet, original_amount+10)
     
 ### Write main function
 def main():
@@ -222,7 +225,7 @@ def main():
     stall2=Stall("Fruitfully Delicious", inventory=inventory2, cost=15, earnings=600)
     cashier1=Cashier("Carly", directory =[stall1, stall2])
     cashier2=Cashier("Amy", directory=[stall2])
-
+    
     
 
     
